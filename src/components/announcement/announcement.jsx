1fc7e0cc41c11f0dc4c424203ea199f8d9fe3e5a
허강 <phreshoutof@gmail.com>
2024-06-07 18:22:50 +0900

    사이드바 추가

--- src/components/announcement/announcement.jsx
+++ src/components/announcement/announcement.jsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import styles from '../../styles/announcement.module.css';
 import AnnouncementItem from './announcementitem.jsx'
+import Link from "next/link";
 
 export default function Announcement(props){
-    
+
     const [announcements, setAnnouncements] = useState([])
 
     useEffect(()=>{
@@ -42,36 +43,36 @@ export default function Announcement(props){
     }
 
     return (
-        <div id="announce-container" className={styles.announceContainer}>
-            <div id="announce-header" className={styles.announceHeader}>
+        <div className={styles.announceContainer}>
+            <div className={styles.announceHeader}>
                 <p>공지사항</p>
             </div>
-            <div id="announce-mid" className={styles.announceMid}>
-                <ul id="search-category" className={styles.searchCategory}>
+            <div className={styles.announceMid}>
+                <ul className={styles.searchCategory}>
                     <li className={`${styles.searchCategoryItem} ${styles.selected}`} data-id="1">전체</li>
                     <li className={styles.searchCategoryItem} data-id="2">서비스</li>
                     <li className={styles.searchCategoryItem} data-id="3">업데이트</li>
                     <li className={styles.searchCategoryItem} data-id="4">이벤트</li>
                 </ul>
-                <form id="search-bar" className={styles.searchBar}>
+                <form className={styles.searchBar}>
                     <input type="text" name="searchTextarea" id="search-textarea" className={styles.searchTextarea}
                            placeholder="검색어 입력"/>
-                    <svg id="search-icon" className={styles.searchIcon} viewBox="0 0 512 512">
+                    <svg className={styles.searchIcon} viewBox="0 0 512 512">
                         <path
                             d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"/>
                     </svg>
                 </form>
             </div>
-            <div id="announce-main" className={styles.announceMain}>
-                <ul id="announce" className={styles.announce}>
+            <div className={styles.announceMain}>
+                <ul className={styles.announce}>
                     {announcements.map(announcement => (
                         <AnnouncementItem key={announcement.announcementId} details={announcement}/>
                     ))}
                 </ul>
-                <div id="announce-bottom" className={styles.announceBottom}>
+                <div className={styles.announceBottom}>
                     <div></div>
-                    <div id="load-more" className={styles.loadMore} onClick={loadMoreData}>더보기</div>
-                    <div id="write-announce" className={styles.writeAnnounce}>글작성</div>
+                    <div className={styles.loadMore} onClick={loadMoreData}>더보기</div>
+                    <Link className={styles.writeAnnounce} href='cs/write'>글작성</Link>
                 </div>
             </div>
         </div>
