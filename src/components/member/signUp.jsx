a18f53d77f89d7f30909d356fca141ab703aabd9
Kang_Tae_seok <goxotjr@naver.com>
2024-06-07 18:31:34 +0900

    ㅎㅇㅎㅇ

--- src/components/member/signUp.jsx
+++ src/components/member/signUp.jsx
@@ -1,14 +1,17 @@
 import React, { useState } from 'react';
-import {singUp} from "../../axiosApi/MemberAxios";
-import {useRouter} from "next/dist/client/router"
-import {handleAxiosError} from "../../axiosApi/errorAxiosHandler";
+import { signUp } from "../../axiosApi/MemberAxios";
+import { useRouter } from 'next/router';
+import { handleAxiosError } from "../../axiosApi/errorAxiosHandler";
 
 const SignUpForm = () => {
     const router = useRouter();
     const [formData, setFormData] = useState({
-        email: '',
-        password: '',
+        userEmail: '',
+        userName: '',
+        userPwd: '',
         confirmPassword: '',
+        phone: '',
+        nickname: '',
     });
 
     const handleInputChange = (e) => {
@@ -21,34 +24,60 @@ const SignUpForm = () => {
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        const { email, password, confirmPassword } = formData;
-        if (password !== confirmPassword) {
+        const { userEmail, userName, userPwd, confirmPassword, phone, nickname } = formData;
+        if (userPwd !== confirmPassword) {
             alert('비밀번호가 일치하지 않습니다.');
             return;
         }
-        const singUpData = {
-            email:email,
-            password:password,
+        const signUpData = {
+            userEmail,
+            userName,
+            userPwd,
+            phone,
+            nickname,
+            provider: "intelliclass",
+            registerTime: new Date().toISOString(),
+            profileImageUrl: "",
+            userType: 0,
+            reportCount: 0,
+            loginOk: 'Y',
+            faceLoginYn: 'N',
+            snsAccessToken: "",
+        };
+        try {
+            await signUp(signUpData);
+            router.push("/user/login");
+        } catch (error) {
+            handleAxiosError(error);
         }
-        singUp(singUpData).then(res =>{
-            router.push("/user/login")
-        }).catch(handleAxiosError)
     };
 
     return (
         <div className="center-div">
             <form className="form" onSubmit={handleSubmit}>
                 <div className="form-group">
-                    <label htmlFor="email">이메일:</label>
-                    <input type="email" id="email" name="email" value={formData.email} onChange={handleInputChange} required />
+                    <label htmlFor="userEmail">이메일:</label>
+                    <input type="email" id="userEmail" name="userEmail" value={formData.userEmail} onChange={handleInputChange} required />
                 </div>
                 <div className="form-group">
-                    <label htmlFor="password">비밀번호:</label>
-                    <input type="password" id="password" name="password" value={formData.password} onChange={handleInputChange} required />
+                    <label htmlFor="userName">이름:</label>
+                    <input type="text" id="userName" name="userName" value={formData.userName} onChange={handleInputChange} required />
                 </div>
                 <div className="form-group">
-                    <label htmlFor="confirmPassword">확인:</label>
+                    <label htmlFor="userPwd">비밀번호:</label>
+                    <input type="password" id="userPwd" name="userPwd" value={formData.userPwd} onChange={handleInputChange} required />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="confirmPassword">비밀번호 확인:</label>
                     <input type="password" id="confirmPassword" name="confirmPassword" value={formData.confirmPassword} onChange={handleInputChange} required />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="phone">휴대폰 번호:</label>
+                    <input type="text" id="phone" name="phone" value={formData.phone} onChange={handleInputChange} required />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="nickname">닉네임:</label>
+                    <input type="text" id="nickname" name="nickname" value={formData.nickname} onChange={handleInputChange} required />
                 </div>
                 <div className="button-container">
                     <button type="submit">회원가입</button>
