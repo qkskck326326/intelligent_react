5e9156d81403b2a2711811202cd8db8c67a55d6a
qkskck326326 <160984009+qkskck326326@users.noreply.github.com>
2024-06-07 11:41:48 +0900

    axios 수정

--- src/pages/itNewsBoard/index.jsx
+++ src/pages/itNewsBoard/index.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import BoardList from "../../components/itNews/boardList";
+//import BoardList from "../../components/itNews/boardList";
 
 const Index = () => {
     return (
         <div>
             <h1>itNewsBoard</h1>
-            <BoardList/>
+
         </div>
     );
 };
