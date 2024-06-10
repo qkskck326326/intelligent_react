032ba252eb9e00d8e7a31e596fe476c55d7df54f
Kang_Tae_seok <goxotjr@naver.com>
2024-06-10 09:49:46 +0900

    ㅎㅇㅎㅇ

--- src/stores/authStore.js
+++ src/stores/authStore.js
@@ -1,24 +1,28 @@
 import {makeAutoObservable} from 'mobx';
 // mobx를 모듈의 로그인상태 자동확인 설정
 // _app.js 에 임포트하여 어딜 가서든 따라다니게 만듬
-class AuthStore {
-    loggedIn = false;
+class AuthStore{
+    isLoggedIn = false;
     isAdmin = false;
-
-    constructor(){
+    constructor() {
         makeAutoObservable(this);
     }
 
-    setLoggedIn(status){
-        this.loggedIn = status
+    setIsLoggedIn(status){
+        this.isLoggedIn = status
+    }
+
+    checkIsLoggedIn(){
+        this.isLoggedIn = !!localStorage.getItem("token");
+        return this.isLoggedIn;  // 로그인 상태를 반환
     }
 
     setIsAdmin(status){
-        this.isAdmin = status
+        this.isAdmin = status;
     }
 
-    checkLoggedIn(){
-        this.loggedIn = !!localStorage.getItem("token");
+    checkIsAdmin(){
+        return this.isAdmin; // 관리자인지 여부를 반환
     }
 }
 
