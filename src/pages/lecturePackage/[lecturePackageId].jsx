220a989257806aa7322f4322ebf5da2c03a7ddab
chaelim <eoett88@gmail.com>
2024-06-12 18:30:19 +0900

    각각 패키지리스트의 상세 조회 가능합니다.

---
+++ src/pages/lecturePackage/[lecturePackageId].jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { useRouter } from 'next/router';
+import LecturePackageDetail from "../../components/lecturePackage/lecturePackageDetail";
+
+const lecturePackageDetailPage = () => {
+    const router = useRouter();
+    const {lecturePackageId} = router.query;
+    console.log(router.query);
+    console.log(parseInt(lecturePackageId, 10));
+    // no가 존재하는 경우에만 BoardDetail 컴포넌트를 렌더링합니다.
+    return lecturePackageId ? <LecturePackageDetail lecturePackageId={parseInt(lecturePackageId, 10)} /> : <div>Loading...</div>;
+};
+
+export default lecturePackageDetailPage;
