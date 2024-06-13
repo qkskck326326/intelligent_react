e3ece9ed546c7ca183eb28ce74f2cbb603a0c912
허강 <phreshoutof@gmail.com>
2024-06-13 18:54:04 +0900

    이것도

--- src/components/chat/chat.jsx
+++ src/components/chat/chat.jsx
@@ -134,7 +134,7 @@ const Chat = observer(({ chatOption, isExpanding, onNavigateToIcon }) => {
 
             {/*챗지피티의 경우 이거 안뜸 시작 */}
 
-            { (chatOption !== 'gpt') &&
+            { (chatOption === 'gpt') &&
                 <div className={`${styles.announceContainer} ${isSearchButtonClicked ? styles.pushed: ''}`}>
                     <span className={styles.horn}>
                         <svg xmlns="http://www.w3.org/2000/svg"
@@ -143,7 +143,7 @@ const Chat = observer(({ chatOption, isExpanding, onNavigateToIcon }) => {
                                 d="M480 32c0-12.9-7.8-24.6-19.8-29.6s-25.7-2.2-34.9 6.9L381.7 53c-48 48-113.1 75-181 75H192 160 64c-35.3 0-64 28.7-64 64v96c0 35.3 28.7 64 64 64l0 128c0 17.7 14.3 32 32 32h64c17.7 0 32-14.3 32-32V352l8.7 0c67.9 0 133 27 181 75l43.6 43.6c9.2 9.2 22.9 11.9 34.9 6.9s19.8-16.6 19.8-29.6V300.4c18.6-8.8 32-32.5 32-60.4s-13.4-51.6-32-60.4V32zm-64 76.7V240 371.3C357.2 317.8 280.5 288 200.7 288H192V192h8.7c79.8 0 156.5-29.8 215.3-83.3z"/>
                         </svg>
                     </span>
-                    <span className={`${styles.announce} ${announceExpand ? styles.expanded : ''}`}>d런거 assdfgsdfgdfa sdfasd kfhasdkfjahskdjfhaksdjfhaksjdfk를 저장해볼rmfjasdfsdfasldkfjalsdkf까요 zzzzzadsfasdfa</span>
+                    <span className={`${styles.announce} ${announceExpand ? styles.expanded : ''}`}>공지사항 공지사항공지사항 공지사항공지사항 공지사항공지사항 공지사항공지사항 공지사항공지사항 공지사항공지사항 공지사항</span>
                     <button className={`${styles.carot}`} onClick={handleAnnounce}>
                         <svg className={`${announceExpand ? commonStyles.rotate180 : commonStyles.rotate180Back}`}
                              xmlns="http://www.w3.org/2000/svg"
