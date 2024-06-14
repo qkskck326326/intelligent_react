2350476dac5b424fd7e5fc62afaedd181f9c8c09
doha12 <c3179923@gmail.com>
2024-06-14 18:15:12 +0900

    커밋

--- src/components/post/PostUploadBtn.jsx
+++ src/components/post/PostUploadBtn.jsx
@@ -1,10 +1,26 @@
 import Link from "next/link";
+import React from "react";
+import { observer } from "mobx-react-lite";
+import authStore from "../../stores/authStore";
 import styles from "./UploadButton.module.css"; // Import the CSS file
 
-const UploadButton = () => (
-  <Link href="/post/PostUploadPage" className={styles.uploadButton}>
-    업로드
-  </Link>
-);
+const UploadButton = observer(({ onLoginRequired }) => {
+  const handleClick = (e) => {
+    if (!authStore.checkIsLoggedIn()) {
+      e.preventDefault();
+      if (onLoginRequired) {
+        onLoginRequired();
+      }
+    }
+  };
+
+  return (
+    <div className={styles.uploadButton} onClick={handleClick}>
+      <Link href="/post/PostUploadPage" passHref>
+        <span>업로드</span>
+      </Link>
+    </div>
+  );
+});
 
 export default UploadButton;
--- src/components/post/postList.jsx
+++ src/components/post/postList.jsx
@@ -3,16 +3,23 @@ import Link from "next/link";
 import { axiosClient } from "../../axiosApi/axiosClient";
 import { Pagination } from "react-bootstrap";
 import styles from "./PostList.module.css";
-import { FaThumbsUp, FaComment, FaEye } from "react-icons/fa";
-import PostUploadBtn from "./PostUploadBtn";
+import { TbEyeSearch } from "react-icons/tb";
+import { AiOutlineLike, AiOutlineComment } from "react-icons/ai";
 import PostSearch from "../../components/post/postSearchBar";
+import LoginPopup from "../../components/post/LoginPopup"; // 새로운 팝업 컴포넌트 임포트
+import authStore from "../../stores/authStore"; // AuthStore 임포트
+import { observer } from "mobx-react-lite";
+import UploadButton from "../../components/post/PostUploadBtn"; // UploadButton 컴포넌트 임포트
+import { getRelativeTime } from "../../components/post/timeUtils"; // 유틸리티 함수 임포트
+import CategoryToggle from "../../components/post/CategoryToggle";
 
-const PostList = () => {
+const PostList = observer(({ selectedCategory, onSelectCategory }) => {
   const [posts, setPosts] = useState([]);
   const [loading, setLoading] = useState(true);
   const [page, setPage] = useState(0);
   const [size, setSize] = useState(10);
   const [totalPages, setTotalPages] = useState(0);
+  const [showPopup, setShowPopup] = useState(false); // 팝업 상태
 
   useEffect(() => {
     const fetchPosts = async () => {
@@ -23,8 +30,9 @@ const PostList = () => {
         });
         setPosts(res.data.content || []);
         setTotalPages(res.data.totalPages || 0);
+        console.log(res.data.content);
       } catch (error) {
-        console.error("Error fetching posts:", error);
+        console.error("게시물 로드 중 오류 발생:", error);
       } finally {
         setLoading(false);
       }
@@ -37,9 +45,50 @@ const PostList = () => {
     setPage(pageNumber);
   };
 
+  const handlePostClick = (e, postId) => {
+    if (!authStore.checkIsLoggedIn()) {
+      e.preventDefault();
+      setShowPopup(true);
+    }
+  };
+
+  const handleUploadClick = () => {
+    setShowPopup(true);
+  };
+
+  const handleSelectCategory = (category) => {
+    setSelectedCategory(category);
+    // 하위 카테고리를 선택했을 때 추가 동작이 필요하다면 여기에 추가하십시오.
+  };
+
+  // const getRelativeTime = (timestamp) => {
+  //   const now = new Date();
+  //   const postDate = new Date(timestamp);
+  //   const diffInSeconds = Math.floor((now - postDate) / 1000);
+
+  //   if (diffInSeconds < 60) {
+  //     return `${diffInSeconds}초 전`;
+  //   } else if (diffInSeconds < 3600) {
+  //     const diffInMinutes = Math.floor(diffInSeconds / 60);
+  //     return `${diffInMinutes}분 전`;
+  //   } else if (diffInSeconds < 86400) {
+  //     const diffInHours = Math.floor(diffInSeconds / 3600);
+  //     return `${diffInHours}시간 전`;
+  //   } else if (diffInSeconds < 604800) {
+  //     const diffInDays = Math.floor(diffInSeconds / 86400);
+  //     return `${diffInDays}일 전`;
+  //   } else {
+  //     return postDate.toLocaleDateString();
+  //   }
+  // };
+
   return (
     <div className={styles.postListContainer}>
       <h1 className={styles.title}>Posts</h1>
+      <CategoryToggle
+        selectedCategory={selectedCategory}
+        onSelectCategory={handleSelectCategory}
+      />
       <PostSearch />
       {loading ? (
         <h2 className={styles.loading}>Loading...</h2>
@@ -47,33 +96,46 @@ const PostList = () => {
         <ul className={styles.postList}>
           {posts.map((post) => (
             <li key={post.id} className={styles.postItem}>
-              {console.log("postId:", post.id)}
               <Link href={`/post/${post.id}`} passHref>
-                <div className={styles.postHeader}>
-                  <div className={styles.postAvatar}></div>
-                  <div className={styles.postMeta}>
-                    <span className={styles.postNickname}>{post.nickname}</span>
-                    <span className={styles.postCategory}>
-                      {post.categoryName}
-                    </span>
+                <div
+                  className={styles.postLink}
+                  onClick={(e) => handlePostClick(e, post.id)}
+                >
+                  <div className={styles.postHeader}>
+                    <div className={styles.postAvatar}>
+                      <img
+                        src={post.profileImageUrl}
+                        className={styles.profileImage}
+                      />
+                    </div>
+                    <div className={styles.postMeta}>
+                      <span className={styles.postNickname}>
+                        {post.nickname}
+                      </span>
+                      <span className={styles.postCategory}>
+                        {post.categoryName}
+                      </span>
+                    </div>
+                    <div className={styles.postTime}>
+                      {getRelativeTime(post.postTime)}
+                    </div>
                   </div>
-                  <div className={styles.postTime}>
-                    {new Date(post.postTime).toLocaleDateString()}
-                  </div>
-                </div>
-                <h5 className={styles.postTitle}>{post.title}</h5>
-                <p className={styles.postSnippet}>{post.contentSnippet}....</p>
-                <div className={styles.postFooter}>
-                  <div className={styles.postStats}>
-                    <span>
-                      <FaThumbsUp /> {post.likeCount}
-                    </span>
-                    <span>
-                      <FaComment /> {post.commentCount}
-                    </span>
-                    <span>
-                      <FaEye /> {post.viewCount}
-                    </span>
+                  <h5 className={styles.postTitle}>{post.title}</h5>
+                  <p className={styles.postSnippet}>
+                    {post.contentSnippet}....
+                  </p>
+                  <div className={styles.postFooter}>
+                    <div className={styles.postStats}>
+                      <span>
+                        <AiOutlineLike size={25} /> {post.likeCount}
+                      </span>
+                      <span>
+                        <AiOutlineComment size={25} /> {post.commentCount}
+                      </span>
+                      <span>
+                        <TbEyeSearch size={25} /> {post.viewCount}
+                      </span>
+                    </div>
                   </div>
                 </div>
               </Link>
@@ -109,10 +171,11 @@ const PostList = () => {
         />
       </Pagination>
       <div className={styles.uploadButton}>
-        <PostUploadBtn />
+        <UploadButton onLoginRequired={handleUploadClick} />
       </div>
+      <LoginPopup show={showPopup} handleClose={() => setShowPopup(false)} />
     </div>
   );
-};
+});
 
 export default PostList;
