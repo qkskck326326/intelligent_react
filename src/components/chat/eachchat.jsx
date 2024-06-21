371735511050de10b8b904fc7329ab506bace630
허강 <phreshoutof@gmail.com>
2024-06-21 16:05:08 +0900

    유저 카운트 정보 추가

--- src/components/chat/eachchat.jsx
+++ src/components/chat/eachchat.jsx
@@ -9,14 +9,15 @@ const EachChat = observer(({chat, onClick}) => {
         <div className={styles.eachChatContainer} onClick={onClick}>
             <div className={styles.pictureFrame}>
                 <div className={styles.eachPicture}>
-                    <img
-                        src="https://i.namu.wiki/i/fzYUOs5_DPeR_vLIWOokJIxZmU1E18nJqpGstH_ivHyPerrkoFoAjfeBP8gaIKUhqCTgfTOP257WDc3r_Yl9nC0fO7K0DBoV6pqkoPmJqSb8IBRbNE7UPbnOEPhfrya2JbP1VjhgMyxHKbImRVujPg.webp"
-                        alt=""/>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 650 650 ">
+                        <path
+                            d="M96 128a128 128 0 1 1 256 0A128 128 0 1 1 96 128zM0 482.3C0 383.8 79.8 304 178.3 304h91.4C368.2 304 448 383.8 448 482.3c0 16.4-13.3 29.7-29.7 29.7H29.7C13.3 512 0 498.7 0 482.3zM609.3 512H471.4c5.4-9.4 8.6-20.3 8.6-32v-8c0-60.7-27.1-115.2-69.8-151.8c2.4-.1 4.7-.2 7.1-.2h61.4C567.8 320 640 392.2 640 481.3c0 17-13.8 30.7-30.7 30.7zM432 256c-31 0-59-12.6-79.3-32.9C372.4 196.5 384 163.6 384 128c0-26.8-6.6-52.1-18.3-74.3C384.3 40.1 407.2 32 432 32c61.9 0 112 50.1 112 112s-50.1 112-112 112z"/>
+                    </svg>
                 </div>
 
-                { chat.chatUser.isPinned === 1 &&
+                {chat.chatUser.isPinned === 1 &&
                     <svg className={styles.pin} xmlns="http://www.w3.org/2000/svg"
-                          viewBox="0 0 384 512">
+                         viewBox="0 0 384 512">
                         <path
                             d="M32 32C32 14.3 46.3 0 64 0H320c17.7 0 32 14.3 32 32s-14.3 32-32 32H290.5l11.4 148.2c36.7 19.9 65.7 53.2 79.5 94.7l1 3c3.3 9.8 1.6 20.5-4.4 28.8s-15.7 13.3-26 13.3H32c-10.3 0-19.9-4.9-26-13.3s-7.7-19.1-4.4-28.8l1-3c13.8-41.5 42.8-74.8 79.5-94.7L93.5 64H64C46.3 64 32 49.7 32 32zM160 384h64v96c0 17.7-14.3 32-32 32s-32-14.3-32-32V384z"/>
                     </svg>
@@ -25,7 +26,7 @@ const EachChat = observer(({chat, onClick}) => {
             </div>
             <div className={styles.middle}>
                 <div className={styles.content}>
-                    <div className={styles.name}>
+                <div className={styles.name}>
                         {chat.chatroom.roomName}
                     </div>
                     {
