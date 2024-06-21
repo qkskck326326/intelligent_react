cc67be2837fe29ec1718138bf55b628cdc272c22
허강 <phreshoutof@gmail.com>
2024-06-21 09:45:03 +0900

    .

--- src/components/chat/bubble.jsx
+++ src/components/chat/bubble.jsx
@@ -113,7 +113,9 @@ const Bubble = observer(({index, onAnnouncementChange, onReport, option, message
                 </div>
                 <div className={`${styles.end} ${isMe ? styles.reverseEnd : ''}`}>
                     <div className={styles.howManyRead}>
-                        {message.readCount}
+                        {message.messageContent !== '삭제된 메시지입니다.' &&
+                            message.readCount
+                        }
                     </div>
                     <div className={styles.time}>
                         {new Date(message.dateSent).toLocaleTimeString('ko-KR').slice(0, -3)}
