93b09c205fb10ade6b2c6628ef2753839ee45e12
qkskck326326 <160984009+qkskck326326@users.noreply.github.com>
2024-06-27 18:19:36 +0900

    2024-06-27 youtube+textlize 페이지 추가

---
+++ src/pages/youtube+textlize/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import TextlizeYoutube from "../../components/itNews/textlize";
+
+const Index = () => {
+
+    return(
+        <TextlizeYoutube/>
+    );
+}
+
+export default Index
