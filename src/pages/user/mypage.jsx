449438913ef875616f746db4b697ab79953f255c
Kang_Tae_seok <goxotjr@naver.com>
2024-06-27 18:33:50 +0900

    태석

--- src/pages/user/mypage.jsx
+++ src/pages/user/mypage.jsx
@@ -8,6 +8,7 @@ import MyAttendance from "../../components/user/myAttendance";
 import MyInfo from "../../components/user/myInfo";
 import MyLecture from "../../components/user/myLecture";
 import LectureManagement from "../../components/user/lectureManagement";
+import PaymentHistoryManagement from "../payment/paymentHistoryManagement";
 
 import styles from "../../styles/user/mypage/mypage.module.css";
 import MypageSidebar from "../../components/user/mypageSidebar";
@@ -36,6 +37,9 @@ const Mypage = observer(() => {
                 return <MyLecture />;
             case "lectureManagement":
                 return <LectureManagement />;
+            case "paymentManagement":
+                return <PaymentHistoryManagement />;
+            
             // 다른 컴포넌트들도 여기 추가
             default:
                 return <MyInfo />;
