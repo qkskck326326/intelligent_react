af11056791aa3eaf6e13b2b04a371c8814cf3d28
qkskck326326 <160984009+qkskck326326@users.noreply.github.com>
2024-06-27 18:18:21 +0900

    2024-06-27 AlertModal.jsx 추가

---
+++ src/components/common/AlertModal.jsx
@@ -0,0 +1,24 @@
+// AlertModal.js
+import React, { useState } from 'react';
+import { Modal, Button } from 'react-bootstrap';
+
+const AlertModal = ({ show, handleClose, title, message }) => {
+    return (
+        <Modal show={show} onHide={handleClose}>
+            <Modal.Header closeButton>
+                <Modal.Title>{title}</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>{message}</Modal.Body>
+            <Modal.Footer>
+                <Button variant="secondary" onClick={handleClose}>
+                    Close
+                </Button>
+                <Button variant="primary" onClick={handleClose}>
+                    Save Changes
+                </Button>
+            </Modal.Footer>
+        </Modal>
+    );
+};
+
+export default AlertModal;
