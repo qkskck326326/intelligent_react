2a432b5e739f9040a79936156e5639cb346b46f0
rudalsdl <lll0803lll@naver.com>
2024-06-28 14:33:58 +0900

    lecture

--- src/components/user/lectureManagement.jsx
+++ src/components/user/lectureManagement.jsx
@@ -4,25 +4,31 @@ import { observer } from "mobx-react";
 import authStore from "../../stores/authStore";
 import { axiosClient } from "../../axiosApi/axiosClient";
 import styles from "../../styles/user/mypage/myLecturePackage.module.css";
+import Link from "next/link";
 
 const MyLecturePackage = observer(() => {
     const router = useRouter();
     const [lecturePackages, setLecturePackages] = useState([]);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
+    const [currentPage, setCurrentPage] = useState(0);
+    const [totalPages, setTotalPages] = useState(0);
 
     useEffect(() => {
         if (!authStore.checkIsLoggedIn()) {
             router.push("/user/login");
         } else {
-            fetchLecturePackages();
+            fetchLecturePackages(currentPage);
         }
-    }, []);
+    }, [currentPage]);
 
-    const fetchLecturePackages = async () => {
+    const fetchLecturePackages = async (page) => {
         try {
-            // const response = await axiosClient.get(`/lecturePackages/user/${authStore.getNickname()}`);
-            setLecturePackages(response.data);
+            const response = await axiosClient.get(`/lecture/lecturePackage/${authStore.getNickname()}`, {
+                params: { page: page, size: 6 }
+            });
+            setLecturePackages(response.data.content);
+            setTotalPages(response.data.totalPages);
         } catch (err) {
             setError(err);
         } finally {
@@ -30,36 +36,145 @@ const MyLecturePackage = observer(() => {
         }
     };
 
+    const handlePageChange = (page) => {
+        setCurrentPage(page);
+    };
+
     if (loading) return <p>로딩 중...</p>;
     if (error) return <p>에러: {error.message}</p>;
 
+    const renderStars = (rating) => {
+        const fullStars = Math.floor(rating);
+        const halfStar = rating % 1 >= 0.5;
+    
+        return (
+            <>
+              {Array(fullStars)
+                  .fill(null)
+                  .map((_, index) => (
+                      <span key={`full-${index}`} className={styles.star}>
+                  ⭐
+                </span>
+                  ))}
+              {halfStar && <span className={styles.halfStar} />}
+            </>
+        );
+      };
+
+    const getLectureLevel = (level) => {
+        switch (level) {
+            case 0:
+                return "입문";
+            case 1:
+                return "기본";
+            case 2:
+                return "심화";
+            default:
+                return "알 수 없음";
+        }
+    };
+
+    const renderLevelIcon = (level) => {
+        return (
+            <div className={styles.levelIcon}>
+              {Array.from({ length: 3 }).map((_, index) => (
+                  <span
+                      key={index}
+                      className={index < level + 1 ? styles.active : styles.inactive}
+                  />
+              ))}
+            </div>
+        );
+      };
+
     return (
         <div className={styles.container}>
-            <h1>My Lecture Packages</h1>
+            <h1>나의 강좌 목록</h1>
             <div className={styles.grid}>
                 {lecturePackages.map((lecture) => (
-                    <div key={lecture.id} className={styles.cardContainer}>
-                        <div className={styles.card}>
-                            <div className={styles.thumbnail}>
-                                <img src={lecture.thumbnail} alt={lecture.title} />
+                    <div key={lecture.lecturePackageId} className={styles.cardContainer}>
+                        <div className={styles.thumbnail}>
+                            <img src={lecture.thumbnail} alt={lecture.title} />
+                        </div>
+                        <div className={styles.details}>
+                            <div className={styles.title}>
+                            <Link href={`/lecturePackage/${lecture.lecturePackageId}`}>
+                                <span className={styles.customLink}>{lecture.title}</span>
+                            </Link>
                             </div>
-                            <div className={styles.details}>
-                                <div className={styles.title}>
-                                    {lecture.title}
+                            <div className={styles.rating}>
+                                {"별점 "}
+                                {lecture.rating ? (
+                                    renderStars(lecture.rating)
+                                ) : (
+                                    <span className={styles.emptyStar}>&nbsp;</span>
+                                )}
                                 </div>
-                                <div className={styles.rating}>
-                                    {"별점 "}
-                                    {lecture.rating}
-                                </div>
-                                <div className={styles.info}>
-                                    {lecture.viewCount} views
-                                </div>
+                            <div className={styles.info}>
+                                <span>
+                                    <img
+                                        className={styles.viewCount}
+                                        src="/images/view_count_icon.png"
+                                        alt="조회수 아이콘"
+                                    />
+                                </span>{" "}
+                                {lecture.viewCount} views
+                                <span className={styles.packageLevel}>
+                                {renderLevelIcon(lecture.packageLevel)}
+                                    {getLectureLevel(lecture.packageLevel)}
+                                </span>
                             </div>
                         </div>
                     </div>
                 ))}
             </div>
+            <div className={styles.paginationContainer}>
+        <ul className={styles.paginationWrapper}>
+          <li>
+            <button
+              className={`${styles.pageLink} ${styles.pageLinkNav}`}
+              onClick={() => handlePageChange(0)}
+            >
+              «
+            </button>
+          </li>
+          <li>
+            <button
+              className={`${styles.pageLink} ${styles.pageLinkNav}`}
+              onClick={() => handlePageChange(currentPage > 0 ? currentPage - 1 : 0)}
+            >
+              ‹
+            </button>
+          </li>
+          {Array.from({ length: totalPages }, (_, i) => (
+            <li key={i + 1}>
+              <button
+                className={`${styles.pageLink} ${i === currentPage ? styles.activePage : ''}`}
+                onClick={() => handlePageChange(i)}
+              >
+                {i + 1}
+              </button>
+            </li>
+          ))}
+          <li>
+            <button
+              className={`${styles.pageLink} ${styles.pageLinkNav}`}
+              onClick={() => handlePageChange(currentPage < totalPages -1 ? currentPage + 1 : totalPages - 1)}
+            >
+              ›
+            </button>
+          </li>
+          <li>
+            <button
+              className={`${styles.pageLink} ${styles.pageLinkNav}`}
+              onClick={() => handlePageChange(totalPages - 1)}
+            >
+              »
+            </button>
+          </li>
+        </ul>
         </div>
+      </div>
     );
 });
 
